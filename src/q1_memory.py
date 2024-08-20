25379cbe738c710e799e4e22217b1d1fc8bebfea
Pablo Galilea <pgalilea.p@gmail.com>
2024-08-20 15:46:34 -0400

    feat: add problem 1

--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,5 +1,30 @@
 from typing import List, Tuple
 from datetime import datetime
 
+from json import loads
+
+
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    """."""
+
+    result = {}
+
+    with open(file_path) as f:
+        for line in f:
+            dct_line = loads(line)
+            tweet_date = datetime.strptime(dct_line["date"][:10], r'%Y-%m-%d').date()
+            # tweet_date = dct_line["date"][:10]
+            user = dct_line["user"]["username"]
+
+            if tweet_date in result.keys():
+                if user in result[tweet_date].keys():
+                    result[tweet_date][user] = result[tweet_date][user] + 1
+                else:
+                    result[tweet_date][user] = 0
+            else:
+                result[tweet_date] = {}
+
+    for d in result.keys():
+        result[d] = max(result[d], key=result[d].get)
+        
+    return list(result.items())
--- src/q1_time.py
+++ src/q1_time.py
@@ -1,5 +1,35 @@
 from typing import List, Tuple
 from datetime import datetime
 
+import polars as pl
+
+
 def q1_time(file_path: str) -> List[Tuple[datetime.date, str]]:
-    pass
+    """."""
+
+    df = (
+        pl.scan_ndjson(
+            file_path,
+            schema={
+                "url": pl.Utf8,
+                "date": pl.Datetime,
+                "user": pl.Struct({"username": pl.Utf8}),
+            },
+        )
+        .with_columns(
+            user=pl.col("user").struct["username"], date=pl.col("date").dt.date()
+        )
+        .group_by(["date", "user"])
+        .len(name="count")
+        .with_columns(pl.col("count").sum().over("date").alias("sum_tweets"))
+        .sort(["date", "count"], descending=True)
+        .group_by("date")
+        .first()
+        .select(["date", "user"])
+        .sort("date")
+    )
+
+    # print(df.collect().head(20))
+    # print(df.collect().head(20).rows())
+
+    return df.collect().rows()
